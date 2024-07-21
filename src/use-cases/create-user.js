d3db88cf8173c8173ab15fe6852fcf4fbd6fa12c
Renanmdr <renanmdr3@gmail.com>
2024-07-21 17:29:11 -0300

    feat: add PostgresGetUserByEmailRepository

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,9 +1,19 @@
 import bcrypt from 'bcrypt'
 import { randomUUID } from 'node:crypto'
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
 export class CreateUserUseCase {
   async execute(createUserParams) {
     const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+    const postgresGetUserByEmailRepository =
+      new PostgresGetUserByEmailRepository()
+    const userWithProvidedEmail =
+      await postgresGetUserByEmailRepository.execute(createUserParams.email)
+
+    if (userWithProvidedEmail) {
+      throw new Error('The provided e-mail is already in use')
+    }
 
     const user = {
       ...createUserParams,
