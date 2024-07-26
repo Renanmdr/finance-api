7bd7e6c973b11349f34975c499a8bffe99a28137
Renanmdr <renanmdr3@gmail.com>
2024-07-26 19:33:30 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,25 @@
+import { randomUUID } from 'node:crypto'
+import { UserNotFoundError } from '../../errors/user'
+export class CreateTransactionUseCase {
+  constructor(createTransactionRepository, getUserByIdRepository) {
+    this.createTransactionRepository = createTransactionRepository
+    this.getUserByIdRepository = getUserByIdRepository
+  }
+
+  async execute(createTransactionParams) {
+    const userId = createTransactionParams.userId
+
+    const user = await this.getUserByIdRepository.execute(userId)
+
+    if (!user) {
+      throw new UserNotFoundError(userId)
+    }
+
+    const transaction = await this.createTransactionRepository({
+      ...createTransactionParams,
+      id: randomUUID(),
+    })
+
+    return transaction
+  }
+}
