b283839d964c4aace59e96cc925e147b0dc60312
Renanmdr <renanmdr3@gmail.com>
2024-07-28 12:48:04 -0300

    feat: add GetTransactionsByUserIdUseCase

---
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,21 @@
+import { UserNotFoundError } from '../../errors/user.js'
+export class GetTransactionsByUserIdUseCase {
+  constructor(getTransactionsByUserIdRepository, getUserByIdRepository) {
+    this.GetUserByIdRepository = getUserByIdRepository
+    this.getTransactionsByUserIdRepository = getTransactionsByUserIdRepository
+  }
+
+  async execute(params) {
+    const user = await this.GetUserByIdRepository.execute(params.userId)
+
+    if (!user) {
+      throw new UserNotFoundError()
+    }
+
+    const transactions = await this.getTransactionsByUserIdRepository(
+      params.userId,
+    )
+
+    return transactions
+  }
+}
