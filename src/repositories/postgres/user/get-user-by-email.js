b4099b820376a605e33769a8038fc456608f29a1
Renanmdr <renanmdr3@gmail.com>
2024-08-10 16:23:05 -0300

    feat: ensure CreateUser and GetUserByEmailRepository use Prisma

--- src/repositories/postgres/user/get-user-by-email.js
+++ src/repositories/postgres/user/get-user-by-email.js
@@ -1,12 +1,18 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
+// import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresGetUserByEmailRepository {
   async execute(email) {
-    const user = await PostgresHelper.query(
-      'SELECT * FROM users WHERE email = $1',
-      [email],
-    )
+    return await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    // const user = await PostgresHelper.query(
+    //   'SELECT * FROM User WHERE email = $1',
+    //   [email]
+    // )
 
-    return user[0]
+    // return user[0]
   }
 }
